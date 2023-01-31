8166b85d1420ba01b477479d44b2aaad57b3729f
42 <1358688418@qq.com>
2023-02-01 02:56:05 +0800

    2.1.0

--- chat.py
+++ chat.py
@@ -1,27 +1,14 @@
-from revChatGPT.ChatGPT import Chatbot
+from revChatGPT.Official import Chatbot
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 
 port = 8006
-
 app = FastAPI()
-chatbot = Chatbot(
-    {
-        "email": "邮箱",
-        "password": "密码",
-        "captcha": "2captcha 密钥",
-    }
-)
-
-
-
-# 依赖这两个东西，参照 acheong08/ChatGPT 文档进行配置
-# https://github.com/acheong08/ChatGPT
-# https://2captcha.com/
-
-
-
+chatbot = Chatbot(api_key="") 
+# 只需填入你的 openai api_key 即可。一键直达 https://platform.openai.com/account/api-keys
+# 不再需要 2captcha 密钥
+# 按照 https://github.com/acheong08/ChatGPT 的说法是完全免费的，但未经证实
 
 class ChatRequest(BaseModel):
     prompt: str
@@ -30,9 +17,12 @@ class ChatRequest(BaseModel):
 def chatGPT(request: ChatRequest):
     prompt = request.prompt
     print(prompt)
-    answer = chatbot.ask(prompt)
+    if prompt == "__clear__":
+        chatbot.reset()
+        return {"message": "OK"}
+    answer = chatbot.ask(prompt)["choices"][0]["text"]
     print(answer)
-    return answer
+    return {"message": answer}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
