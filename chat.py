e1f3ca208ea35251b5aec23e14c0363da1363a08
42 <37006258+MirrorCY@users.noreply.github.com>
2023-02-13 04:58:31 +0800

    Update chat.py
    谢谢你 Copyalot

--- chat.py
+++ chat.py
@@ -1,28 +1,40 @@
-from revChatGPT.Official import Chatbot
-from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
+from fastapi import FastAPI,Body
+from revChatGPT.V2 import Chatbot
 
-port = 8006
+port=8006
 app = FastAPI()
-chatbot = Chatbot(api_key="") 
-# 只需填入你的 openai api_key 即可。一键直达 https://platform.openai.com/account/api-keys
-# 不再需要 2captcha 密钥
-# 按照 https://github.com/acheong08/ChatGPT 的说法是完全免费的，但未经证实
+chatbot = Chatbot(
+    email="",
+    password="",
+    # paid=True,
+    # proxy="http://127.0.0.1:7890",
+)
 
-class ChatRequest(BaseModel):
-    prompt: str
+'''
+根据是否开通了 plus 版，选择 paid 参数，如果开通了，就填 True，否则就不填
+根据是否使用代理，选择 proxy 参数，如果你在国内，建议使用代理，否则就不填
+email 填写你的 openai 账号，是邮箱
+password 填写你的 openai 密码
+
+~~以上是内容 copilot 自动生成的~~
+
+~~以下代码是 chatGPT 生成的~~
+'''
+
+async def get_text(prompt):
+    text = ""
+    async for response in chatbot.ask(prompt):
+        text += response["choices"][0]["text"].replace("<|im_end|>", "")
+    return text
+
 
 @app.post("/chat")
-def chatGPT(request: ChatRequest):
-    prompt = request.prompt
-    print(prompt)
-    if prompt == "__clear__":
-        chatbot.reset()
-        return {"message": "OK"}
-    answer = chatbot.ask(prompt)["choices"][0]["text"]
-    print(answer)
+async def chatGPT(body: dict = Body(...)):
+    prompt = body["prompt"]
+    answer = await get_text(prompt)
+    print(prompt + "\n" + answer)
     return {"message": answer}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
+uvicorn.run(app, host="0.0.0.0", port=port)
