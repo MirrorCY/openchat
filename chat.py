c0c467236362f984806036574c7e3ba53c6bc7b4
42 <37006258+MirrorCY@users.noreply.github.com>
2023-02-13 16:40:46 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -12,10 +12,12 @@ chatbot = Chatbot(
 )
 
 '''
-根据是否开通了 plus 版，选择 paid 参数，如果开通了，就填 True，否则就不填
-根据是否使用代理，选择 proxy 参数，如果你在国内，建议使用代理，否则就不填
+注意关注上游更新，每次启动前可以先执行一次 pip3 install --upgrade revChatGPT
+“# ” 是注释的意思，去掉它这一行才生效，python 根据缩进判断逻辑结构，请确保删除注释符号后上下行对其不然会报错
+根据是否开通了 chatGPT Plus 版，选择 paid 参数，如果开通了，就填 True
+根据是否使用代理，选择 proxy 参数，如果你在国内，必须使用代理，否则会报错
 email 填写你的 openai 账号，是邮箱
-password 填写你的 openai 密码
+password 填写你的 openai 密码，是 openai 账户的密码，不是你邮箱的密码
 
 ~~以上是内容 copilot 自动生成的~~
 
